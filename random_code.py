268659dbda43b7ed68bdabc6c54da1f4ed897b01
Mirunalini07 <mirunalinikr7002@gmail.com>
2026-02-15 20:56:43 +0530

    Add files via upload

---
+++ random_code.py
@@ -0,0 +1,35 @@
+import random
+
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?"
+
+length = int(input("Enter password length: "))
+
+if length <= 0:
+    print("Password length must be greater than 0")
+    exit()
+
+use_letters = input("Include letters? (y/n): ")
+use_numbers = input("Include numbers? (y/n): ")
+use_symbols = input("Include symbols? (y/n): ")
+
+characters = ""
+
+if use_letters.lower() == "y":
+    characters += letters
+if use_numbers.lower() == "y":
+    characters += numbers
+if use_symbols.lower() == "y":
+    characters += symbols
+
+if characters == "":
+    print("You must select at least one character type.")
+    exit()
+
+password = ""
+
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
